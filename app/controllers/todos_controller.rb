1655715701159757ced969677d55dbe783671305
Ivo Iturrieta <iaiturri@uc.cl>
2017-09-17 23:23:10 +0000

    add index and links

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -31,6 +31,10 @@ class TodosController < ApplicationController
     end
   end
   
+  def index
+    @todos = Todo.all
+  end
+  
   private
     def todo_params
       params.require(:todo).permit(:name, :description)
